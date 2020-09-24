d63834eb47820ae82433fc189900261b3e2472c4
Duzmath Lajos <68466919+crabyke@users.noreply.github.com>
2020-09-24 21:00:19 +0200

    Sticky navbar
    Sticky navbar

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 // VARIABLES
 
 // EVENT LISTENERS
+window.addEventListener("scroll", scrollFunction);
 
 // FUNCTIONS
+
+function scrollFunction() {
+    let nav = document.getElementById("navbar");
+    let logo = document.getElementById("logo");
+    nav.classList.toggle("sticky", window.scrollY > 0);
+    logo.classList.toggle("transformer", window.scrollY > 0);
+};
